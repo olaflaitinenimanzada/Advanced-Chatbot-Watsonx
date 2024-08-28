e2d42a5815c6f5ad521c220e46496fced8c7eb0c
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 15:41:55 +0200

    Create discovery_integration.py

---
+++ src/discovery/discovery_integration.py
@@ -0,0 +1,9 @@
+from src.discovery.discovery_setup import setup_discovery
+
+def integrate_discovery():
+    discovery = setup_discovery()
+    # Integration code here
+    pass
+
+if __name__ == "__main__":
+    integrate_discovery()
