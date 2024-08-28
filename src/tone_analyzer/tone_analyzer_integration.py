e819b2ef12d7b6f8277ab5cf8f85addfbcf334b5
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 15:41:17 +0200

    Create tone_analyzer_integration.py

---
+++ src/tone_analyzer/tone_analyzer_integration.py
@@ -0,0 +1,9 @@
+from src.tone_analyzer.tone_analyzer_setup import setup_tone_analyzer
+
+def integrate_tone_analyzer():
+    tone_analyzer = setup_tone_analyzer()
+    # Integration code here
+    pass
+
+if __name__ == "__main__":
+    integrate_tone_analyzer()
