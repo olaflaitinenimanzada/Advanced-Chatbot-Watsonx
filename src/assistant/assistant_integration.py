2e4a637c7ea45ef85161f638323f24af2cb832c6
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 15:40:06 +0200

    Create assistant_integration.py

---
+++ src/assistant/assistant_integration.py
@@ -0,0 +1,9 @@
+from src.assistant.assistant_setup import setup_assistant
+
+def integrate_assistant():
+    assistant = setup_assistant()
+    # Integration code here
+    pass
+
+if __name__ == "__main__":
+    integrate_assistant()
