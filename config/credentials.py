606e022ee630034ad6e50ba0d2d57c0ed9161de0
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 15:39:25 +0200

    Create credentials.py

---
+++ config/credentials.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ['WATSON_ASSISTANT_API_KEY'] = 'your_assistant_api_key'
+os.environ['WATSON_ASSISTANT_URL'] = 'your_assistant_url'
+os.environ['WATSON_NLU_API_KEY'] = 'your_nlu_api_key'
+os.environ['WATSON_NLU_URL'] = 'your_nlu_url'
+os.environ['WATSON_TONE_ANALYZER_API_KEY'] = 'your_tone_analyzer_api_key'
+os.environ['WATSON_TONE_ANALYZER_URL'] = 'your_tone_analyzer_url'
+os.environ['WATSON_DISCOVERY_API_KEY'] = 'your_discovery_api_key'
+os.environ['WATSON_DISCOVERY_URL'] = 'your_discovery_url'
